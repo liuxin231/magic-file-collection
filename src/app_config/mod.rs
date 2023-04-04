c5dbb55bde9a84d6a904d755d7977d308391783f
liuxin <dove__lx@aliyun.com>
2023-04-04 19:33:20 +0800

    update watcher file from single to multiple.

--- src/app_config/mod.rs
+++ src/app_config/mod.rs
@@ -15,7 +15,8 @@ pub struct Log {
 #[derive(Debug, Deserialize, Serialize)]
 #[allow(unused)]
 pub struct Notify {
-    pub watching_path: String,
+    pub watching_path: Vec<String>,
+    pub file_name_regex: Vec<String>,
     pub flush_timing: Option<u64>,
     pub offset_file: Option<String>,
 }
